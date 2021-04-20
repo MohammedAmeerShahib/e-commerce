5e0512b1036378ccff37a2edd57f66584cdcd5f9
Ameer Shahib 1995 <MohammedAmeerShahib>
2021-04-20 10:10:06 +0530

    inti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatCarousel, MatCarouselComponent } from 'ng-mat-carousel';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'e-commerce';
+  slides = [{'image': './assets/Images/Carousel/1920x480-water-droplets.jpg'}, {'image': './assets/Images/Carousel/luiz-henrique-brito-galaxy.jpg'},{'image': './assets/Images/Carousel/181231-FQ-New-Brand-NEWS-Web-Banner-1920x480.jpg'}, {'image': './assets/Images/Carousel/next-interactions-banner-4.png'}, {'image': './assets/Images/Carousel//monoTex-5--Headerbild-1920-x-480-Homepage-Produkte.jpg'}];
+
 }
